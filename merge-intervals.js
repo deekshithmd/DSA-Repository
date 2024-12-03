f3cbff1b0b12a55464fe2632ef3ea9fe010eb8b4
deekshithmd <deekshithmogra@gmail.com>
2024-12-03 22:08:37 +0530

    started overlapping intervals

--- merge-intervals.js
+++ merge-intervals.js
@@ -44,3 +44,41 @@ function mergeIntervalsInplace(intervals) {
 
 console.log('Merge intervals...', mergeIntervals([[7, 9], [1, 3], [6, 8], [2, 5],]))
 console.log('Merge intervals inplace...', mergeIntervalsInplace([[7, 9], [1, 3], [6, 8], [2, 5],]))
+
+// insert interval, given non overlapping intervals
+function insertInterval(intervals, intervalToInsert) {
+    let finalIntervals = [], i = 0;
+
+    while (i < intervals.length && intervals[i][1] < intervalToInsert[0]) {
+        finalIntervals.push(intervals[i])
+        i++;
+    }
+
+    while (i < intervals.length && intervals[i][0] <= intervalToInsert[1]) {
+        intervalToInsert[0] = Math.min(intervalToInsert[0], intervals[i][0]);
+        intervalToInsert[1] = Math.max(intervalToInsert[1], intervals[i][1]);
+        i++;
+    }
+
+    finalIntervals.push(intervalToInsert)
+
+    while (i < intervals.length) {
+        finalIntervals.push(intervals[i])
+        i++
+    }
+    return finalIntervals
+}
+
+console.log('Final set of intervals....', insertInterval([[1, 3], [5, 7], [8, 12]], [4, 6]))
+
+// find intersection of intervals
+
+function intersectionOfIntervals(firstIntervals, secondIntervals) {
+    let result = [], i = 0, j = 0;
+
+    while (i < firstIntervals.length && j < secondIntervals.length) {
+        firstOverlappesSecond = firstIntervals[i][0] >= secondIntervals[j][0] && firstIntervals[i][1] <= secondIntervals[j][1]
+    }
+}
+
+console.log('Intersection of intervals...', intersectionOfIntervals([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]]))
