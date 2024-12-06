9306f55ee5901fd0c0c3aae37f96c5f4cc31d6fc
deekshithmd <deekshithmogra@gmail.com>
2024-12-06 22:19:29 +0530

    added duplicate numbers

--- cyclic-sort.js
+++ cyclic-sort.js
@@ -37,3 +37,47 @@ function findMissingNumber(arr) {
     return n
 }
 console.log('Missing number..........', findMissingNumber([4, 0, 1, 3]))
+
+//Find all missing numbers - (1-n)
+function findAllMissingNumbers(arr) {
+    let missingNumbers = [], i = 0, n = arr.length;
+    while (i < n) {
+        const destIndex = arr[i] - 1;
+        if (arr[i] !== arr[destIndex]) {
+            [arr[i], arr[destIndex]] = [arr[destIndex], arr[i]]
+        }
+        else {
+            i++
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if (arr[i] !== i + 1) {
+            missingNumbers.push(i + 1)
+        }
+    }
+    return missingNumbers
+}
+console.log('Missing numbers..........', findAllMissingNumbers([4, 3, 2, 2, 6, 6]))
+
+// Find the duplicate number
+function findTheDuplicateNumber(arr) {
+    let i = 0, n = arr.length;
+    while (i < n) {
+        if (arr[i] !== i + 1) {
+            const destIndex = arr[i] - 1;
+            if (arr[i] !== arr[destIndex]) {
+                [arr[i], arr[destIndex]] = [arr[destIndex], arr[i]]
+            }
+            else {
+                return arr[i]
+            }
+        }
+        else {
+            i++
+        }
+    }
+
+    return -1
+}
+
+console.log('Duplicate number is....', findTheDuplicateNumber([1, 2, 4, 3, 3]))
